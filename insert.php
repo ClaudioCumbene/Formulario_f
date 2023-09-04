ccb613a336fc61d8b4dcb5ea90abd5d13d8969d4
Claudio Cumbene <claudiocumbene@gmail.com>
2023-09-04 10:52:50 +0200

    guardando a pagina de insercao de dados na pagina

---
+++ insert.php
@@ -0,0 +1,24 @@
+<?php 
+	require_once 'db-config.php';
+	$q="insert into utilizador (nome, email, perfil, username, password) values (?,?,?,?,?)";
+	$stmt = $db->prepare($q);
+	$stmt->bindParam(1, $nome);
+	$stmt->bindParam(2, $email);
+	$stmt->bindParam(3, $perfil);
+	$stmt->bindParam(4, $username);
+	$stmt->bindParam(5, $password);
+
+	$nome=$_POST['nome'];
+	$perfil=$_POST['perfil'];
+	$email=$_POST['email'];
+	$username=$_POST['username'];
+	$password=$_POST['password'];
+
+	if ($stmt->execute()) {
+		header("Location:index.php");
+	}
+
+	
+
+ ?>
+ <p>Colorado</p>
