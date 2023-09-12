e2e37aa702a82425ad15586dd6bc803775f8b250
Claudio Cumbene <claudiocumbene@gmail.com>
2023-09-12 13:51:41 +0200

    Testando a requisicao

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const num= prompt('insira o teu nome ');
 let nomeAtribuido= 'Joao de Abreu';
 
 console.log("num");
+
+if(num==nomeAtribuido){
+console.log("num");
+}else{
+alert('Nome invalido');
+
+}
+
+//alert('Pagina invalida');
